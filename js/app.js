88d4fe89cc1272bc66276877dfac03cc65950f57
Andonys24 <andonihernandez1990@gmail.com>
2024-06-07 19:25:54 -0600

    Implementacion de Footer - Pagina Responsi - Solucion de Bugs con la barra de busqueda y menu de hamburguesa

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
+// Mostar Input de busqueda
 let search = document.querySelector('.search-box');
+
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    // Remover activo del menu si se activa el input de busqueda
+    menu.classList.remove('active');
 }
+
+// Mostar y ocultar menu de hamburguesa
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+    // remover input de busqueda cuando se inicie el menu
+    search.classList.remove('active');
+}
+
+// eliminar menu al hacer scroll o darl click a una opcion
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+
+// Header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 80);
+})
